fe45ee20c4ce95e7e7cb9f7c9cf2d6c42af34467
Pzmatty <48995891+Pzmatty@users.noreply.github.com>
2019-03-29 15:43:54 -0300

    Main con ejemplos
    Varios objetos request con ejemplos para operaciones de Remedy.

--- src/claro/ticketremedy/ws/TicketRemedyServices.java
+++ src/claro/ticketremedy/ws/TicketRemedyServices.java
@@ -26,45 +26,42 @@ public class TicketRemedyServices {
 	
 	
 	public static void main(String[] args) {		 
-		//TicketRemedyRequest request = new TicketRemedyRequest("RMDInsert?","TEMIPARG","HPD:Help Desk","'1000000000'='NOMBRE RESUMEN CPD'", null,null,null);
-		//System.out.println("salida Respuesta:     " + request.toString());
 		
-		//'1000000018'='SAFE ARG' '1000000019'='CUENTA GENERICA' '1000000251'='CLARO ARGENTINA' '1000000076'='CREATE' '1000000014'='NOC' '1000000217'='ARG-MV-SOPORTE DE CONMUTACION' '1000000000'='TEST3 FRAN BORRAR AHORA' '1000000151'='øEs PP Reg o Cta Seg? Cta SeguraøPP o Reg? Loc Pcia y ALM en la cual emite ALM a la que pertenece Copiar Mercado de Stealth MIN y Billing del Cliente DescripciÛn del Problema Mensaje recibido Fecha y hora de intentos øEst· en Roaming?SI/NO MIN /DN: 5470441011 Tipo de Negocio: PP Mercado: AMBA Tipo de equipo: CPP' '303497400'='CO008U100MB2' '1000000163'='3000' '1000000162'='4000' '1000000099'='0' '1000000251'='CLARO ARGENTINA' '1000000215'='5000' '301572100'='TESTFIELD'	
-		/*	TicketRemedyRequest(String service,
-		String sistema,
-		String forma,
-		String columnas, 
-		String condiciones, 
-		String adicionales,
-		String id,
-		String orden)*/
-		
-		
-		TicketRemedyRequest request = new TicketRemedyRequest(
+	// Ejemplo para realizar un insert en Remedy, devuelve un n√∫mero de ID de Remedy como <nuevo></nuevo> en el XML
+	/*	TicketRemedyRequest request = new TicketRemedyRequest(
 				"RMDInsert?", // Servicio
 				"TEMIPARG", // Sistema
 				"HPD:Help Desk", // Forma
-				"'1000000019'='CUENTA GENERICA' '1000000151'='···········aaaaaaa '1000000215'='5000' '536890043'='CPD' '304306840'='STL PROBLEMAS DE COBERTURA' '1000000099'='0' '536890008'='0' '301572100'='NPAR' '1000000163'='3000' '1000000162'='4000' '1000000000'='STL PROBLEMAS DE COBERTURA'",
+				"'1000000019'='CUENTA GENERICA' '1000000151'='------√≥√≥√≥√≥√≥√≥√≥√≥√≥------ '1000000215'='5000' '536890043'='CPD' '304306840'='STL PROBLEMAS DE COBERTURA' '1000000099'='0' '536890008'='0' '301572100'='NPAR' '1000000163'='3000' '1000000162'='4000' '1000000000'='STL PROBLEMAS DE COBERTURA'",
 				null, // Condiciones
-				null, // Adicionales
-				null, // Id (Update),
-				null // orden
-				);
-		
-	//	'1000000018'='STEALTH ARG' '1000000019'='CUENTA GENERICA' '1000000151'='PRUEBA ERROR ARGENTINA 11' '1000000215'='5000' '536890043'='CPD' '304306840'='GPON CORTE DE SERVICIO' '1000000099'='0' '536890008'='0' '301572100'='ARG' '1000000000'='Desciption'
-		
-	/*	TicketRemedyRequest request = new TicketRemedyRequest(
-				"RMDSelect?", // Servicio
-				"STEALTHARG", // Sistema
-				"HPD:Help Desk", // Forma
-				"1000000161", // Columnas
-				"'7'=' (5) ' '1000000019'='CUENTA GENERICA' '1000000018'='STEALTH ARG' '1000000251'='CLARO ARGENTINA' '301572100'='NPAR'", // Condiciones
 				null, // Adicionales
 				null, // Id (Update),
 				null // orden
 				);*/
 		
+	// Ejemplo para realizar un select en Remedy, devuelve varios incidentes en el XML
+//	TicketRemedyRequest request = new TicketRemedyRequest(
+//			"RMDSelect?", // Servicio
+//			"STEALTHARG", // Sistema
+//			"HPD:Help Desk", // Forma
+//			"1000000161", // Columnas
+//			"'7'=' (5) ' '1000000019'='CUENTA GENERICA' '1000000018'='STEALTH ARG' '1000000251'='CLARO ARGENTINA' '301572100'='NPAR'", // Condiciones
+//			null, // Adicionales
+//			null, // Id (Update),
+//			null // orden
+//			);
 
+		// Esta consulta est√°tica devuelve las plantillas de Remedy (sigue siendo una URL para hacer select pero devuelve otros datos).	
+		/*TicketRemedyRequest request = new TicketRemedyRequest(
+				"RMDSelect?", // Servicio
+				"STEALTHARG", // Sistema
+				"HPD:Template", // Forma
+				"1000001437&20303165800¬†303165900 303174700", // Columnas
+				"'1000000580'='1'", // Condiciones
+				null, // Adicionales
+				null, // Id (Update),
+				null // orden
+				);*/
 	
 		TicketRemedyResponse response = consultarTickets(request);
 		System.out.println("Respuesta:     " + response.toString());
